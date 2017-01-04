f5f06e58e22cde6c9ce7be2671388d2cacbf7427
Tomas Mancal <tomas@FU-184.karlov.mff.cuni.cz>
2017-01-04 19:28:48 +0100

    Tests of f2py with pgi compilers

--- setup.py
+++ setup.py
@@ -1,2 +1,24 @@
 # -*- coding: utf-8 -*-
+import os
+from numpy.distutils.misc_util import Configuration
+from numpy.distutils.core import setup
 
+def configuration(parent_package='',top_path=None):
+    config = Configuration(None,parent_package,top_path)
+    libraries = [
+            #'gomp',
+            #'blas',
+            ]
+    config.add_extension('aceto',
+                         ['lib/trp2.f95'],
+                         libraries = libraries,
+                         f2py_options =[''],
+                         define_macros = [('F2PY_REPORT_ON_ARRAY_COPY','1')],
+                         # this is the flag gfortran needs to process OpenMP directives
+                         extra_compile_args = ['-fopenmp '],
+                         extra_link_args = ['-fopenmp '],
+                         )
+    return config
+
+if __name__ == "__main__":
+    setup(configuration=configuration)
