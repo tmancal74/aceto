25504120bea6967e0d9c57d739a4a8ff93de8be6
Tomas Mancal <tomas@FU-184.karlov.mff.cuni.cz>
2017-02-14 15:44:20 +0100

    Integrating aceto with quantarhei

--- aceto/quantarhei_test.py
+++ aceto/quantarhei_test.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from quantarhei import Manager
+
 import numpy
 import scipy
 import time
@@ -8,32 +10,40 @@ from band_system import band_system
 
 import nr3td
 
+
 #
 # Define system
 #
-Ns = [1,2,1]
+#Ns = [1,2,1]
+#sys = band_system(3, Ns)
+#
+#en = numpy.array([0.0, 0.92, 1.08, 2.0], dtype=numpy.float64)
+#rwa = 1.0
+#sys.set_energies(en)
+#
+#d_ge = numpy.zeros((3,1,2), dtype=numpy.float64, order='F')
+#d_ef = numpy.zeros((3,2,1), dtype=numpy.float64, order='F')
+#d_ge[:,0,1] = numpy.array([0.0, 1.0, 0.0],dtype=numpy.float64)
+#d_ge[:,0,0] = numpy.array([1.0/numpy.sqrt(2.0), 1.0/numpy.sqrt(2.0), 0.0],dtype=numpy.float64)
+#d_ef[:,1,0] = numpy.array([0.0, 3.0, 0.0],dtype=numpy.float64)
+#d_ef[:,0,0] = numpy.array([0.0, 3.0, 0.0],dtype=numpy.float64)
+#sys.set_dipoles(0,1,d_ge)
+#sys.set_dipoles(1,2,d_ef)
+#
+#Kr = numpy.zeros((2,2), dtype=numpy.float64)
+#Kr[0,0] = -1.0/50.0
+#Kr[1,0] = 1.0/50.0
+#Kr[0,1] = 1.0/50.0
+#Kr[1,1] = -1.0/50.0
+#sys.init_dephasing_rates()
+#sys.set_relaxation_rates(1,Kr)
+#
+
+#
+# Create band_system from quantarhei classes
+#
 sys = band_system(3, Ns)
-
-en = numpy.array([0.0, 0.92, 1.08, 2.0], dtype=numpy.float64)
 rwa = 1.0
-sys.set_energies(en)
-
-d_ge = numpy.zeros((3,1,2), dtype=numpy.float64, order='F')
-d_ef = numpy.zeros((3,2,1), dtype=numpy.float64, order='F')
-d_ge[:,0,1] = numpy.array([0.0, 1.0, 0.0],dtype=numpy.float64)
-d_ge[:,0,0] = numpy.array([1.0/numpy.sqrt(2.0), 1.0/numpy.sqrt(2.0), 0.0],dtype=numpy.float64)
-d_ef[:,1,0] = numpy.array([0.0, 3.0, 0.0],dtype=numpy.float64)
-d_ef[:,0,0] = numpy.array([0.0, 3.0, 0.0],dtype=numpy.float64)
-sys.set_dipoles(0,1,d_ge)
-sys.set_dipoles(1,2,d_ef)
-
-Kr = numpy.zeros((2,2), dtype=numpy.float64)
-Kr[0,0] = -1.0/50.0
-Kr[1,0] = 1.0/50.0
-Kr[0,1] = 1.0/50.0
-Kr[1,1] = -1.0/50.0
-sys.init_dephasing_rates()
-sys.set_relaxation_rates(1,Kr)
 
 #
 # define lab settings
@@ -76,7 +86,3 @@ om3 = 2.0*scipy.pi*numpy.fft.fftshift(numpy.fft.fftfreq(len(t3s), d=dt)) + rwa
 
 plt.contourf(om1,om3,numpy.real(ftresp),100)
 plt.show()
-
-
-
-
