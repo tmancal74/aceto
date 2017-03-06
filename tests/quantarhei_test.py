1a271634778bb75ba842a58d2366d91885d5eeac
tmancal74 <tmancal74@gmail.com>
2017-03-06 17:14:20 +0100

    Calculation of 2D spectrum moved to Quantarhei

--- tests/quantarhei_test.py
+++ tests/quantarhei_test.py
@@ -8,6 +8,9 @@ from quantarhei import eigenbasis_of
 
 from quantarhei import PopulationPropagator
 
+from quantarhei.spectroscopy.twod2 import TwoDSpectrumContainer
+from quantarhei.spectroscopy.twod2 import TwoDSpectrumCalculator
+
 import numpy
 #import scipy
 import time
@@ -86,237 +89,47 @@ with energy_units("1/cm"):
 agg.build(mult=2)
 
 
-###############################################################################
-#
-# Create band_system from quantarhei classes
-#
-###############################################################################
-
-#
-# hamiltonian and transition dipole moment operators
-#
-H = agg.get_Hamiltonian()
-D = agg.get_TransitionDipoleMoment()
-
-#
-# Construct band_system object
-#
-Nb = 3
-Ns = numpy.zeros(Nb, dtype=numpy.int)
-Ns[0] = 1
-Ns[1] = agg.nmono
-Ns[2] = Ns[1]*(Ns[1]-1)/2
-sys = band_system(Nb, Ns)
-
-
-#
-# Set energies
-#
-en = numpy.zeros(sys.Ne, dtype=numpy.float64)
-#if True:
-with eigenbasis_of(H):
-    for i in range(sys.Ne):
-        en[i] = H.data[i,i]
-    sys.set_energies(en)
-
-    #
-    # Set transition dipole moments
-    #
-    dge_wr = D.data[0:Ns[0],Ns[0]:Ns[0]+Ns[1],:]
-    def_wr = D.data[Ns[0]:Ns[0]+Ns[1],(Ns[0]+Ns[1]):(Ns[0]+Ns[1]+Ns[2]),:]
-
-    dge = numpy.zeros((3,Ns[0],Ns[1]), dtype=numpy.float64)
-    deff = numpy.zeros((3,Ns[1],Ns[2]), dtype=numpy.float64)
-    
-    for i in range(3):
-        dge[i,:,:] = dge_wr[:,:,i]
-        deff[i,:,:] = def_wr[:,:,i]
-    sys.set_dipoles(0,1,dge)
-    sys.set_dipoles(1,2,deff)
-
-
-#
-# Relaxation rates
-#
-KK = agg.get_RedfieldRateMatrix()
-
-# relaxation rate in single exciton band
-Kr = KK.data[Ns[0]:Ns[0]+Ns[1],Ns[0]:Ns[0]+Ns[1]]*10.0
-#print(1.0/Kr)
-
-sys.init_dephasing_rates()
-sys.set_relaxation_rates(1,Kr)
-
-
-#
-# Lineshape functions
-#
-sbi = agg.get_SystemBathInteraction()
-cfm = sbi.CC
-cfm.create_double_integral()
-
-
-#
-# Transformation matrices
-#
-SS = H.diagonalize()
-SS1 = SS[1:Ns[1]+1,1:Ns[1]+1]
-SS2 = SS[Ns[1]+1:,Ns[1]+1:]
-H.undiagonalize()
-
-sys.set_gofts(cfm._gofts)    # line shape functions
-sys.set_sitep(cfm.cpointer)  # pointer to sites
-sys.set_transcoef(1,SS1)      # matrix of transformation coefficients  
-sys.set_transcoef(2,SS2)      # matrix of transformation coefficients  
 
 
 
 #
-# define lab settings
+# Calculate 2D spectra
 #
-lab = lab_settings(lab_settings.FOUR_WAVE_MIXING)
-X = numpy.array([1.0, 0.0, 0.0], dtype=numpy.float64)
-lab.set_laser_polarizations(X, X, X, X)
-
-#
-# Other parameters
-#
-
-dt = ta.step
-t1s = ta.data 
-t3s = ta.data 
 
 # TimeAxis for t2 waiting time
 t2s = TimeAxis(0.0, 5, 100.0)
+tcalc = TwoDSpectrumCalculator(t1axis=ta, t2axis=t2s, t3axis=ta,
+                               system=agg)
+twods = tcalc.calculate(rwa, verbose=True)
 
 #
-# Finding population evolution matrix
-#
-prop = PopulationPropagator(ta, Kr)
-Uee, Uc0 = prop.get_PropagationMatrix(t2s, corrections=True)
+# Show 2D spectra (save them)
+#    
 
-tc = 0
-teetoos = t2s.data
-for tt2 in teetoos:
+w1_min = 11000.0
+w1_max = 13000.0
+w3_min = 11000.0
+w3_max = 13000.0
 
-    #
-    # Initialize response storage
-    #
-    resp_r = numpy.zeros((Nr, Nr), dtype=numpy.complex128, order='F')
-    resp_n = numpy.zeros((Nr, Nr), dtype=numpy.complex128, order='F')
+with energy_units("1/cm"):
 
-    (it2, err) = ta.locate(tt2) 
-    print("t2 = ", tt2, "fs (it2 = ", it2,")")
-    
-    #
-    # calcute response
-    #
-    print("calculating response: ")
-    rmin = 0.0001
-    t1 = time.time()
-    
-    print(" - ground state bleach")
-    # GSB
-    nr3td.nr3_r3g(lab, sys, it2, t1s, t3s, rwa, rmin, resp_r) 
-    nr3td.nr3_r4g(lab, sys, it2, t1s, t3s, rwa, rmin, resp_n)
-
-    print(" - stimulated emission")
-    # SE
-    nr3td.nr3_r1g(lab, sys, it2, t1s, t3s, rwa, rmin, resp_n)
-    nr3td.nr3_r2g(lab, sys, it2, t1s, t3s, rwa, rmin, resp_r)
-    
-    print(" - excited state absorption")
-    # ESA
-    nr3td.nr3_r1fs(lab, sys, it2, t1s, t3s, rwa, rmin, resp_r)
-    nr3td.nr3_r2fs(lab, sys, it2, t1s, t3s, rwa, rmin, resp_n)
-    
-    # Transfer
-    sys.set_population_propagation_matrix(Uee[:,:,tc]-Uc0[:,:,tc])
-    
-    print(" - stimulated emission with transfer")    
-    # SE
-    nr3td.nr3_r1g_trans(lab, sys, it2, t1s, t3s, rwa, rmin, resp_n)
-    nr3td.nr3_r2g_trans(lab, sys, it2, t1s, t3s, rwa, rmin, resp_r)
-
-    print(" - excited state absorption with transfer") 
-    # ESA
-    nr3td.nr3_r1fs_trans(lab, sys, it2, t1s, t3s, rwa, rmin, resp_r)
-    nr3td.nr3_r2fs_trans(lab, sys, it2, t1s, t3s, rwa, rmin, resp_n)
-    
-    
-    t2 = time.time()
-    print("... calculated in ",t2-t1, " sec")
-    
-    
-    #
-    # Calculate corresponding 2D spectrum
-    #
-    
-    ftresp = numpy.fft.fft(resp_r,axis=1)
-    ftresp = numpy.fft.ifft(ftresp,axis=0)
-    reph2D = numpy.fft.fftshift(ftresp)
-    
-    ftresp = numpy.fft.ifft(resp_n,axis=1)
-    ftresp = numpy.fft.ifft(ftresp,axis=0)*ftresp.shape[1]
-    nonr2D = numpy.fft.fftshift(ftresp)
-    
-    atype = ta.atype
-    ta.atype = 'complete'
-    oa1 = ta.get_FrequencyAxis() 
-    oa1.data += rwa
-    oa1.start += rwa
-    ta.atype = atype
-    
-    atype = ta.atype
-    ta.atype = 'complete'
-    oa3 = ta.get_FrequencyAxis() 
-    oa3.data += rwa
-    oa3.start += rwa
-    ta.atype = atype
-    
-        
-
-    #
-    # Show 2D specrtrum
-    #
-
-    tot2D_real = numpy.real(reph2D) + numpy.real(nonr2D)
-    
-    if tc == 0:
-        max_tot = numpy.max(tot2D_real)
-            
-    w1_min = 11000.0
-    w1_max = 13000.0
-    w3_min = 11000.0
-    w3_max = 13000.0
-    
-    with energy_units("1/cm"):
+    k = 0
+    for tt2 in t2s.data:
         #
         # Plotting with given units on axes
         #
-        #print("Frequency span w1: ", oa1.min, oa1.max)      
-        (i1_min, dist) = oa1.locate(w1_min)
-        (i1_max, dist) = oa1.locate(w1_max)
-        #print("Frequency span w3: ", oa3.min, oa3.max)      
-        (i3_min, dist) = oa3.locate(w3_min)
-        (i3_max, dist) = oa3.locate(w3_max)    
-    
-        realout = numpy.real(tot2D_real[i1_min:i1_max,i3_min:i3_max])/max_tot
-    
-        Ncontour = 100
-        plt.contourf(oa1.data[i1_min:i1_max],oa3.data[i3_min:i3_max],
-                     realout, Ncontour)
-    
-    
-    
-    
-    
-    t_end = time.time()
-    print("Finished at ", t_end-t_start, " secs")
-    figname = "fig"+str(round(tt2))+".png"
-    print(figname)
-    plt.savefig(figname)
-    
-    #plt.show()
+        twods[k].plot(axis=[w1_min, w1_max, w3_min, w3_max])
 
-    tc += 1
+        figname = "fig"+str(round(tt2))+".png"
+        print("saving file: ", figname, " with 2D spectrum at ", tt2, "fs")
+        plt.savefig(figname)
+        #plt.show()
+        
+        k += 1
+
+t_end = time.time()
+print("Finished at ", t_end-t_start, " secs") 
+
+
+
+
