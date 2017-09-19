5b76f496a669fbeeb1afdfc268bf6b750a7c9560
Tomáš Mančal <tomas@Tomass-MacBook-Pro.local>
2017-09-19 22:47:27 +0200

    quantarhei test update

--- tests/quantarhei_test.py
+++ tests/quantarhei_test.py
@@ -41,7 +41,7 @@ with energy_units("1/cm"):
     mol1 = Molecule(elenergies=[0.0, 12000.0])
     mol2 = Molecule(elenergies=[0.0, 12200.0])
     mol3 = Molecule(elenergies=[0.0, 12400.0])
-    mol4 = Molecule(elenergies=[0.0, 12800.0])
+    mol4 = Molecule(elenergies=[0.0, 12500.0])
 mol1.position = [0.0, 0.0, 0.0]
 mol2.position = [0.0, 10.0, 0.0]
 mol3.position = [10.0, 0.0, 0.0]
@@ -56,13 +56,23 @@ mol4.set_dipole(0,1,v1)
 # rwa frequency as an average transition frequency
 rwa = (mol1.elenergies[1]+mol2.elenergies[1])/2.0
 
+lab = lab_settings(lab_settings.FOUR_WAVE_MIXING)
+a_0 = numpy.array([1.0, 0.0, 0.0], dtype=numpy.float64)
+a_90 = numpy.array([0.0, 1.0, 0.0], dtype=numpy.float64)
+a_m45 = numpy.array([-1.0/numpy.sqrt(2.0), 
+                     1.0/numpy.sqrt(2.0), 0.0], dtype=numpy.float64)
+a_p45 = numpy.array([1.0/numpy.sqrt(2.0), 
+                     1.0/numpy.sqrt(2.0), 0.0], dtype=numpy.float64)
+lab.set_laser_polarizations(a_0,a_90,a_m45,a_p45)
+
+
 #
 # System-bath interaction
 #      
 
 tsbi = TimeAxis(0.0, 3*Nr, 2.0)
 
-params = dict(ftype="OverdampedBrownian", T=300, reorg=200.0, cortime=100.0)
+params = dict(ftype="OverdampedBrownian", T=300, reorg=50.0, cortime=100.0)
 with energy_units('1/cm'):
     cf = CorrelationFunction(tsbi, params)
     
@@ -88,8 +98,9 @@ with energy_units("1/cm"):
 
 agg.build(mult=2)
 
-
-
+with energy_units("1/cm"):
+    rwa_cm = agg.get_RWA_suggestion()
+rwa = agg.get_RWA_suggestion()
 
 
 #
@@ -97,19 +108,22 @@ agg.build(mult=2)
 #
 
 # TimeAxis for t2 waiting time
-t2s = TimeAxis(0.0, 5, 50.0)
+t2s = TimeAxis(0.0, 5, 200.0)
 tcalc = TwoDSpectrumCalculator(t1axis=ta, t2axis=t2s, t3axis=ta,
                                system=agg)
-twods = tcalc.calculate(rwa, verbose=True)
+twods = tcalc.calculate(rwa, verbose=True, lab=lab)
 
 #
 # Show 2D spectra (save them)
 #    
 
-w1_min = 11000.0
-w1_max = 13000.0
-w3_min = 11000.0
-w3_max = 13000.0
+w1_min = rwa_cm - 700.0
+w1_max = rwa_cm + 700.0
+w3_min = rwa_cm - 700.0
+w3_max = rwa_cm + 700.0
+
+window_2D = [w1_min, w1_max, w3_min, w3_max]
+print(window_2D)
 
 with energy_units("1/cm"):
 
