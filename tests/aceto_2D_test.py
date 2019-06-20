2c93461b19073599e7e9e1d190bb4a47efcece15
tmancal74 <tmancal74@gmail.com>
2019-06-20 12:18:52 +0200

    Adjusting tests to new development in Quantarhei

--- tests/aceto_2D_test.py
+++ tests/aceto_2D_test.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import numpy
 import time
 
+import quantarhei as qr
+
 from quantarhei import Molecule
 from quantarhei import Aggregate
 from quantarhei import energy_units
@@ -13,7 +15,7 @@ from quantarhei import CorrelationFunction
 #from quantarhei import PopulationPropagator
 
 #from quantarhei.spectroscopy.twod2 import TwoDSpectrumContainer
-from quantarhei.spectroscopy.twod2 import TwoDSpectrumCalculator
+from quantarhei import TwoDResponseCalculator
 
 
 from aceto.lab_settings import lab_settings
@@ -23,8 +25,8 @@ from aceto.lab_settings import lab_settings
 t_start = time.time()
 
 # Time axis for t1 and t3 times
-Nr = 500
-ta = TimeAxis(0.0, Nr, 2.0)
+Nr = 100
+ta = TimeAxis(0.0, Nr, 10.0)
 
 ###############################################################################
 #
@@ -51,29 +53,34 @@ v1 = numpy.array([0.3, 0.3, 0.3])
 v1 = 10.0*v1/numpy.sqrt(numpy.dot(v1,v1))
 mol4.set_dipole(0,1,v1)
 
+molecules = [mol1, mol2, mol3, mol4]
+
 # rwa frequency as an average transition frequency
-rwa = (mol1.elenergies[1]+mol2.elenergies[1])/2.0
+rwa = 0.0
+for mol in molecules:
+    rwa += mol.elenergies[1]
+rwa = rwa/len(molecules)
 
 lab = lab_settings(lab_settings.FOUR_WAVE_MIXING)
 a_0 = numpy.array([1.0, 0.0, 0.0], dtype=numpy.float64)
 a_90 = numpy.array([0.0, 1.0, 0.0], dtype=numpy.float64)
-a_m45 = numpy.array([-1.0/numpy.sqrt(2.0), 
+a_m45 = numpy.array([-1.0/numpy.sqrt(2.0),
                      1.0/numpy.sqrt(2.0), 0.0], dtype=numpy.float64)
-a_p45 = numpy.array([1.0/numpy.sqrt(2.0), 
+a_p45 = numpy.array([1.0/numpy.sqrt(2.0),
                      1.0/numpy.sqrt(2.0), 0.0], dtype=numpy.float64)
-lab.set_laser_polarizations(a_0,a_90,a_m45,a_p45)
-
+#lab.set_laser_polarizations(a_0,a_90,a_m45,a_p45)
+lab.set_laser_polarizations(a_0,a_0,a_0,a_0)
 
 #
 # System-bath interaction
-#      
+#
 
-tsbi = TimeAxis(0.0, 3*Nr, 2.0)
+tsbi = TimeAxis(0.0, 3*Nr, 10.0)
 
-params = dict(ftype="OverdampedBrownian", T=300, reorg=50.0, cortime=100.0)
+params = dict(ftype="OverdampedBrownian", T=300, reorg=30.0, cortime=50.0)
 with energy_units('1/cm'):
     cf = CorrelationFunction(tsbi, params)
-    
+
 mol1.set_transition_environment((0,1),cf)
 mol2.set_transition_environment((0,1),cf)
 mol3.set_transition_environment((0,1),cf)
@@ -81,18 +88,19 @@ mol4.set_transition_environment((0,1),cf)
 
 #
 # Creating aggregate
-#      
-agg = Aggregate("Dimer", molecules=[mol1, mol2, mol3, mol4])
+#
+agg = Aggregate(molecules=molecules)
 #agg = Aggregate("Dimer", molecules=[mol1, mol2, mol3])
 
-with energy_units("1/cm"):
-    agg.set_resonance_coupling(0,1, 60.0)
-    agg.set_resonance_coupling(1,2, 60.0)
-    agg.set_resonance_coupling(0,2, 30.0)
-    agg.set_resonance_coupling(1,3, 30.0)
-    pass
+if True:
+    with energy_units("1/cm"):
+        agg.set_resonance_coupling(0,1, 60.0)
+        agg.set_resonance_coupling(1,2, 60.0)
+        agg.set_resonance_coupling(0,2, 30.0)
+        agg.set_resonance_coupling(1,3, 30.0)
+        pass
 
-#agg.set_coupling_by_dipole_dipole()    
+#agg.set_coupling_by_dipole_dipole()
 
 agg.build(mult=2)
 
@@ -107,28 +115,28 @@ rwa = agg.get_RWA_suggestion()
 
 # TimeAxis for t2 waiting time
 
-t2s = TimeAxis(0.0, 2, 10.0)
+t2s = TimeAxis(0.0, 50, 10.0)
 
-tcalc = TwoDSpectrumCalculator(t1axis=ta, t2axis=t2s, t3axis=ta,
+tcalc = TwoDResponseCalculator(t1axis=ta, t2axis=t2s, t3axis=ta,
                                system=agg)
 tcalc.bootstrap(rwa, verbose=True, lab=lab)
 twods = tcalc.calculate()
 
 #
 # Show 2D spectra (save them)
-#    
+#
 
-w1_min = rwa_cm - 700.0
-w1_max = rwa_cm + 700.0
-w3_min = rwa_cm - 700.0
-w3_max = rwa_cm + 700.0
+w1_min = rwa_cm - 1000.0
+w1_max = rwa_cm + 1000.0
+w3_min = rwa_cm - 1000.0
+w3_max = rwa_cm + 1000.0
 
 window_2D = [w1_min, w1_max, w3_min, w3_max]
 print(window_2D)
 
 with energy_units("1/cm"):
 
-    
+
     # find info for normalization to the largest value across the whole set
 #    k = 0
 #    mx = numpy.zeros(len(t2s.data))
@@ -137,44 +145,41 @@ with energy_units("1/cm"):
 #        spect2D = numpy.real(spectrum.reph2D) + numpy.real(spectrum.nonr2D)
 #        mx[k] = numpy.amax(spect2D)
 #        k += 1
-#        
+#
 #    mloc = numpy.argmax(mx)
 #    mval = mx[mloc]
-#    
+#
 #    print(mloc, mval)
 #    mval = 1.0
-#    
+#
 #    normalize = True
 #
 #    # normalize
 #    k = 0
 #    for tt2 in t2s.data:
-#        
+#
 #        if normalize:
 #            twods[k].devide_by(mval)
 #        k += 1
-#        
+#
 #    cbmax = twods[mloc].plot(axis=[w1_min, w1_max, w3_min, w3_max],vmax=1.0)
-    
+
     k = 0
     for tt2 in t2s.data:
-            
+
         #
         # Plotting with given units on axes
         #
         sp = twods.get_spectrum(tt2)
-        sp.plot(axis=[w1_min, w1_max, w3_min, w3_max]) #,vmax=1.0, cbmax=cbmax)
+        sp.get_TwoDSpectrum(dtype=qr.signal_TOTL)
+        sp.plot(window=[w1_min, w1_max, w3_min, w3_max]) #,vmax=1.0, cbmax=cbmax)
 
         figname = "fig"+str(round(tt2))+".png"
         print("saving file: ", figname, " with 2D spectrum at ", tt2, "fs")
         plt.savefig(figname)
         #plt.show()
-        
+
         k += 1
 
 t_end = time.time()
-print("Finished at ", t_end-t_start, " secs") 
-
-
-
-
+print("Finished at ", t_end-t_start, " secs")
